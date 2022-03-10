8fd0b25f6b3abc23c5bb4121061782e1c6cbdce6
AnastasiyaNekrasova <90055861+AnastasiyaNekrasova@users.noreply.github.com>
2022-03-10 10:43:51 +0300

    Update TextFunction.java

--- LR_2/src/by/belstu/it/nekrasova/TextFunction.java
+++ LR_2/src/by/belstu/it/nekrasova/TextFunction.java
@@ -3,6 +3,6 @@ package by.belstu.it.nekrasova;
 public class TextFunction {
     public static void main(String[] args) {
         // Display "Hello World!"
-        System.out.println("Hello World!");
+        System.out.println("Changes from Git");
     }
 }
